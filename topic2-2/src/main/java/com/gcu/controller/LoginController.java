d0f13c64aa57ff38bc95ddc805105e6703476f00
Mikey <migg_18@live.com>
2024-02-28 20:06:28 -0700

    Finished topic 2

--- topic2-2/src/main/java/com/gcu/controller/LoginController.java
+++ topic2-2/src/main/java/com/gcu/controller/LoginController.java
@@ -1,28 +1,65 @@
 package com.gcu.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.gcu.model.LoginModel;
+import com.gcu.model.OrderModel;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
 	/**
-	 * Simple Hello World Controller that returns a String in the response body.
-	 * Invoke using /test1 URI.
+	 * Login Controller that returns a View and Model.
+	 * Invoke using / URI.
 	 * 
-	 * @return Hello World test string
+	 * @return Login form
 	 */
 	@GetMapping("/")
-	@ResponseBody
 	public String display(Model model)
 	{
 		model.addAttribute("title", "Login Form");
 		model.addAttribute("loginModel", new LoginModel());
 		return "login";
+	}
+	
+	/**
+	 * Login Controller that print the form values.
+	 * Invoke using /doLogin URI.
+	 * 
+	 * @return Login View
+	 */
+	@PostMapping("/doLogin")
+	public String doLogin(@Valid LoginModel loginModel, BindingResult bindingResult, Model model) 
+	{
+		// Check for validation errors
+		if (bindingResult.hasErrors())
+		{
+			model.addAttribute("title", "Login Form");
+			return "login";
+		}
+		
+		// Create some Orders
+		List<OrderModel> orders = new ArrayList<OrderModel>();
+		orders.add(new OrderModel(0L, "0000000001", "Product 1", 1.00f, 1));
+		orders.add(new OrderModel(1L, "0000000002", "Product 2", 2.00f, 2));
+		orders.add(new OrderModel(2L, "0000000003", "Product 3", 3.00f, 3));
+		orders.add(new OrderModel(3L, "0000000004", "Product 4", 4.00f, 4));
+		orders.add(new OrderModel(4L, "0000000005", "Product 5", 5.00f, 5));
+		
+		// Setting model attributes
+	    model.addAttribute("title", "My Orders");
+	    model.addAttribute("orders", orders);
+		
+		// Navigate back to orders view
+		return "orders";
 	}
 }
--- topic2-2/src/main/java/com/gcu/model/LoginModel.java
+++ topic2-2/src/main/java/com/gcu/model/LoginModel.java
@@ -1,15 +1,26 @@
 package com.gcu.model;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class LoginModel {
-	private String userName;
+	@NotNull(message="User name is a required field")
+	@Size(min=1, max=32, message="User name must be between 1 and 32 characters")
+	private String username;
+	
+	@NotNull(message="Password is a required field")
+	@Size(min=1, max=32, message="Password must be between 1 and 32 characters")
 	private String password;
 	
-	public String getUserName() {
-		return userName;
+	// Getter and Setter methods for the username
+	public String getUsername() {
+		return username;
 	}
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public void setUsername(String username) {
+		this.username = username;
 	}
+	
+	// Getter and Setter methods for the password
 	public String getPassword() {
 		return password;
 	}
